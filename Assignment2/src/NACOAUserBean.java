ffd8a404e46a169fc1b3dd21a61b8bda5ca8f20c
gzdilos <fasor@outlook.com>
2015-09-25 17:37:41 +1000

    Added userbean that users are beans

---
+++ Assignment2/src/NACOAUserBean.java
@@ -0,0 +1,107 @@
+
+public class NACOAUserBean {
+
+	private int userID;
+	private String username;
+	private String firstname;
+	private String lastname;
+	private String emailAddress;
+	private String password;
+	private String nickname;
+	private String dob;
+	private String address;
+	private String creditDetails;
+	private int isHalted;
+	
+	public NACOAUserBean() {
+		
+	}
+	
+	public void setUserID(int user_id) {
+		this.userID = user_id;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public void setEmailAddress(String email) {
+		this.emailAddress = email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setDOB(String dob) {
+		this.dob = dob;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public void setCreditDetails(String credit) {
+		this.creditDetails = credit;
+	}
+	
+	public void setHalted(int halted) {
+		this.isHalted = halted;
+	}
+	
+	public int getUserID() {
+		return userID;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getDOB() {
+		return dob;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public String getCreditDetails() {
+		return creditDetails;
+	}
+	
+	public int getHalted() {
+		return isHalted;
+	}
+}
